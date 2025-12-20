fbd83ad2eede144c3d622d2ec71d9085ca88e787
Naufal Rifqi <140623816+naufalsetiawan@users.noreply.github.com>
2025-12-20 17:17:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 from PIL import Image
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 
 def is_inside(box_small, box_big):
     # IoA (Intersection over Area)
@@ -91,5 +91,6 @@ webrtc_streamer(
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
     },
 )
+
 
 
